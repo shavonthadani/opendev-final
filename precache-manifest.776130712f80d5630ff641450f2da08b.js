83efa2f4daed7c869c7ca92c88e570b8e3a433fd
shavonthadani <shavon.thadani@gmail.com>
2023-07-18 15:45:04 -0400

    Updates

---
+++ precache-manifest.776130712f80d5630ff641450f2da08b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2dd82902fa816df1d43013eba3f58ef",
+    "url": "/opendev-final/index.html"
+  },
+  {
+    "revision": "6fbcadd00bdc95c1528e",
+    "url": "/opendev-final/static/css/2.70233154.chunk.css"
+  },
+  {
+    "revision": "4219b00365f87423e6e1",
+    "url": "/opendev-final/static/css/main.79dee9bb.chunk.css"
+  },
+  {
+    "revision": "6fbcadd00bdc95c1528e",
+    "url": "/opendev-final/static/js/2.b4baa4fd.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/opendev-final/static/js/2.b4baa4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4219b00365f87423e6e1",
+    "url": "/opendev-final/static/js/main.d3083419.chunk.js"
+  },
+  {
+    "revision": "5814a24f0e3b4ad0b1d7",
+    "url": "/opendev-final/static/js/runtime-main.78faabbb.js"
+  },
+  {
+    "revision": "0b1dd39557592def97a5dfd071f9dc01",
+    "url": "/opendev-final/static/media/accessibility.0b1dd395.jpg"
+  },
+  {
+    "revision": "01a3f97bd49b62051bc0d3ca758a2bbd",
+    "url": "/opendev-final/static/media/chatbot.01a3f97b.jpg"
+  },
+  {
+    "revision": "b7e0a3f042874e538ba3ecbc7c0d0b4c",
+    "url": "/opendev-final/static/media/climate-app.b7e0a3f0.jpg"
+  },
+  {
+    "revision": "8cedf403d47b67ba2e23340cfb261bd3",
+    "url": "/opendev-final/static/media/coding.8cedf403.jpg"
+  },
+  {
+    "revision": "5c9a43bf0ec80d8204b8b9d497d068c5",
+    "url": "/opendev-final/static/media/fitness-tracker.5c9a43bf.jpg"
+  },
+  {
+    "revision": "5bcced964a31498c0366d2579460b870",
+    "url": "/opendev-final/static/media/github.5bcced96.jpg"
+  },
+  {
+    "revision": "8e73b9121675a45a3f26f01c468b3f48",
+    "url": "/opendev-final/static/media/green-energy.8e73b912.jpg"
+  },
+  {
+    "revision": "aa2eb0545a562050984648647866c61f",
+    "url": "/opendev-final/static/media/img-home.aa2eb054.jpg"
+  },
+  {
+    "revision": "9ff8dcb036310c0edda68b16d427a3ee",
+    "url": "/opendev-final/static/media/java.9ff8dcb0.jpg"
+  },
+  {
+    "revision": "bd595f8e54640d6d8fdd012f43fc2d98",
+    "url": "/opendev-final/static/media/meditation-app.bd595f8e.jpg"
+  },
+  {
+    "revision": "4930f6d70604ee0958b93fc225243491",
+    "url": "/opendev-final/static/media/online-learning.4930f6d7.jpg"
+  },
+  {
+    "revision": "482ad670a286e6dad3ad06a033b094f7",
+    "url": "/opendev-final/static/media/python.482ad670.jpg"
+  },
+  {
+    "revision": "78b43520fec8f62f21b4c574afbcedaa",
+    "url": "/opendev-final/static/media/social-justice-events.78b43520.jpg"
+  },
+  {
+    "revision": "e08bc826c037f3e9027c36dc07d5f03f",
+    "url": "/opendev-final/static/media/sustainable-ecommerce.e08bc826.jpg"
+  },
+  {
+    "revision": "99ef93b6330cac260d1798cf19eef5ce",
+    "url": "/opendev-final/static/media/telehealth.99ef93b6.jpg"
+  },
+  {
+    "revision": "6e5602829caf13f70c426ea28d751772",
+    "url": "/opendev-final/static/media/virtual-reality.6e560282.jpg"
+  },
+  {
+    "revision": "d2cbe9901351debebde49b66e675b272",
+    "url": "/opendev-final/static/media/webdev.d2cbe990.jpg"
+  },
+  {
+    "revision": "d43b0803e9638d78b33e83ba5dd176a2",
+    "url": "/opendev-final/static/media/wellness-app.d43b0803.jpg"
+  }
+]);
